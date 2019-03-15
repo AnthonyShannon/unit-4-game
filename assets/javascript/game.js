e93c2c25e38e6a5252851738665d6be669889f20
John Calvin <ashannon1990@gmail.com>
2019-03-14 21:29:57 -0400

    pseudo code added to js, basic html outline created

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Define variables (hp, ap, and counter attack power)
+// Set characters as objects
+// In the objects (characters), assign values to the hp and ap vars
+// For objects, assign "on click animation" to move that character to the battle area.
+    // when chosen character moves to battle are, move remaining characters to a "choose opponent area"
+        // assign "on click animation" to chosen opponent, and move to defender area
+// assign an attack button the feature to subract players attack power form their hp
+// when opponent attacks, subtract their ap from your hp
+// if opponent hp <= 0, select another opponent
+// if your hp <= 0, show text "you lose", reset button appears
+// if no opponents left, show text "you win", reset button appears
+// assign a reset function to reset button
